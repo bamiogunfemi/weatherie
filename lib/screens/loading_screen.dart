c3a888a86b59d678093695f21670499b874d71a4
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-05-17 01:14:25 +0100

    temperature

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -28,12 +28,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
 
 
     var weatherData = await networkHelper.getData();
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
+      return LocationScreen(locationWeather: weatherData,);
     }));
   }
 
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+
+  int temperature;
+  int condition;
+  String cityName;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+  void updateUI(dynamic weatherData){
+
+ temperature = weatherData['main']['temp'];
+condition = weatherData['weather'][0]['id'];
+cityName = weatherData['name'];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,11 +67,11 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      '☀',
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -74,7 +92,3 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
-//double temp = decodedData['main']['temp'];
-//String name = decodedData['name'];
-//print('temp');
-//print('name');
